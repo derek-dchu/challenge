15292f680805c0eb244ce09fa95f9800e8cc7ed4
Derek Hu <derek.dchu@gmail.com>
2015-01-21 01:41:44 -0500

    update / endpoint. Now it serves popular images

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -2,7 +2,8 @@
 
 var express = require('express'),
     fs = require('fs'),
-    Image = require('../models/image');
+    Image = require('../models/image'),
+    api = require('../../lib/500px-api').photos;
 
 var router = express.Router();
 
@@ -14,8 +15,22 @@ module.exports = function (app) {
 };
 
 router.get('/', function (req, res, next) {
+  // Get photos
+  var images = [];
+  var response = {
+    title: page.title
+  };
+  api.getPopular( {'rpp': 20, 'image_size': 2}, function(err, data) {
+    if (err) {
+      response.error = err;
+    } else {
+      data.photos.forEach(function( elem, index, arr ) {
+        images.push( new Image(elem) );
+      });
 
-    res.render('index', {
-      title: page.title
-    });
+      response.images = images;
+    }
+
+    res.render('index', response);
+  });
 });
